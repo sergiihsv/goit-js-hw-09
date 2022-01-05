b8dffd29145565a8eabfdedee5e1e1b7287d9077
sergiihsv <hsv-serg@ukr.net>
2022-01-05 23:20:55 +0200

    1 task  done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,28 @@
 const refs = {
-  bgColorStartBtn: document.querySelector('data-start'),
-  bgColorStopBtn: document.querySelector('data-stop'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
 };
+
+refs.startBtn.addEventListener('click', start);
+refs.stopBtn.addEventListener('click', stop);
+
+let intervalColorChange = '';
+
+function start() {
+  intervalColorChange = setInterval(evt => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.stopBtn.removeAttribute('disabled');
+  refs.startBtn.setAttribute('disabled', true);
+}
+
+function stop() {
+  clearInterval(intervalColorChange);
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
